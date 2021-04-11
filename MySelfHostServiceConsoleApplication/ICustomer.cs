ec67c650fa77c9edf02968c5dc66a3b2d922eb1a
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-04-11 14:00:34 -0300

    Define contrato e o decora com contratos de serviço para expor quais são as operações do serviço

---
+++ MySelfHostServiceConsoleApplication/ICustomer.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace MySelfHostServiceConsoleApplication
+{
+    [ServiceContract]
+    public interface ICustomer
+    {
+        [OperationContract]
+        Customer CreateCustomer(int customerId, string name, string email);
+
+        [OperationContract]
+        Customer GetCustomerByName(string customerName);
+
+        [OperationContract]
+        Customer[] GetAllCustomers();
+    }
+}
