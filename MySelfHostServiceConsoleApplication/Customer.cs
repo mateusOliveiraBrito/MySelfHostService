e2b9f3f675b0a60fc73bc430bbdcbebb36d86979
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-04-11 13:34:00 -0300

    Adicione classe customer para representar um cliente

---
+++ MySelfHostServiceConsoleApplication/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySelfHostServiceConsoleApplication
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CreditCardNumber { get; set; }
+    }
+}
