62d9dc943dd2a6dc8730a9b8762f4f36794d7d89
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-04-11 13:57:56 -0300

    Implementa serviço que herda de Icustomer

---
+++ MySelfHostServiceConsoleApplication/CustomerService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MySelfHostServiceConsoleApplication
+{
+    public class CustomerService : ICustomer
+    {
+        List<Customer> customers = new List<Customer>();
+
+        public CustomerService()
+        {
+            customers = new List<Customer>() {
+                new Customer
+                {
+                    CustomerId = 0,
+                    Name = "Mateus",
+                    Email = "moliveirabrito@gmail.com",
+                    CreditCardNumber = "12345678"
+                },
+                new Customer
+                {
+                    CustomerId = 0,
+                    Name = "Mateus",
+                    Email = "moliveirabrito@gmail.com",
+                    CreditCardNumber = "12345678"
+                },
+                new Customer
+                {
+                    CustomerId = 0,
+                    Name = "Mateus",
+                    Email = "moliveirabrito@gmail.com",
+                    CreditCardNumber = "12345678"
+                },
+                new Customer
+                {
+                    CustomerId = 0,
+                    Name = "Mateus",
+                    Email = "moliveirabrito@gmail.com",
+                    CreditCardNumber = "12345678"
+                }
+                };
+        }
+
+        public Customer CreateCustomer(int customerId, string name, string email)
+        {
+            var customer = new Customer();
+            customer.CustomerId = customerId;
+            customer.Name = name;
+            customer.Email = email;
+
+            return customer;
+        }
+
+        public Customer[] GetAllCustomers()
+        {
+            return customers.ToArray();
+        }
+
+        public Customer GetCustomerByName(string customerName)
+        {
+            return (from c in customers where c.Name == customerName select c).First();
+        }
+    }
+}
