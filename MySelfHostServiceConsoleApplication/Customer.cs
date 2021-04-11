9a56cd8690a58270842c5047ab063b1e2352336b
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-04-11 13:57:04 -0300

    Decora classe para indicar quais membros devem ser expostos

--- MySelfHostServiceConsoleApplication/Customer.cs
+++ MySelfHostServiceConsoleApplication/Customer.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace MySelfHostServiceConsoleApplication
 {
+    [DataContract]
     public class Customer
     {
+        [DataMember]
         public int CustomerId { get; set; }
+
+        [DataMember]
         public string Name { get; set; }
+
+        [DataMember]
         public string Email { get; set; }
+
         public string CreditCardNumber { get; set; }
     }
 }
